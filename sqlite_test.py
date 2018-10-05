a4ee145eef7bdc3e8dda12d14eeac7bbeab8d453
Martin Fägerlind <martin@localhost.localdomain>
2018-10-05 23:04:32 +0200

    Cleanup.

--- sqlite_test.py
+++ sqlite_test.py
@@ -2,34 +2,49 @@ from sqlite_utils import SQLiteDataAdapter, SQLiteDataAdapterConnection
 from matplotlib import pyplot as plt
 import time
 
+import math
+import random as rnd
+
+
+def create_test_table(adapter, table_name, num_rows=100):
+    tquery = time.time()
+
+    # Check if table already exist
+    tables = adapter.get_tables()
+    if table_name in tables:
+        print("Table '{0}' already exists. The table will not be overwritten.".format(table_name))
+        return
+
+    def create_rows(num_rows):
+        i = 0
+        while i < num_rows:
+            x = i
+            y = math.sin(x + rnd.random())
+            yield ({"id": None, "x": x, "y": y})
+            i += 1
+
+    adapter.create_table(table_name)
+    adapter.insert(table_name, create_rows(num_rows))
+    print("Time for creating table: {0}.".format(time.time() - tquery))
+
 def main():
     adapter = SQLiteDataAdapter("./test.db")
-    table_name = "data"
+    table_names = ["table_{0}".format(i) for i in range(5)]
 
-    #tquery = time.time()
-    #adapter.create_test_table(table_name, 10000)
-    #print("Time for creating table: {0}.".format(time.time() - tquery))
+    for table_name in table_names:
+        tquery = time.time()
+        create_test_table(adapter, table_name, 10000)
 
-    tables = adapter.get_tables()
-    print(tables)
+        query = "SELECT x, y from {0} \
+                 WHERE x > 51 AND \
+                 y > 0.0 AND \
+                 y < 0.85".format(table_name)
+        rows = adapter.execute_query(query)
     
-    schema = adapter.get_table_schema(tables[0])
-    print(schema)
-    
-    tquery = time.time()
-    """
-    query = "SELECT x, y from {0} \
-             WHERE x > 51 AND \
-             y > 0.0 AND \
-             y < 0.85".format(table_name)
-    """
-    
-    query = "SELECT x, y from {0} WHERE x*y < 10".format(table_name)
-    rows = adapter.execute_query(query)
-    
-    print("Time for querying table: {0}, number of rows returned {1}".format(time.time() - tquery, len(rows)))
-    print(rows[0])
-    plt.plot(*zip(*rows))
+        print("Time for querying table: {0}, number of rows returned {1}".format(time.time() - tquery, len(rows)))
+        plt.figure()
+        plt.plot(*zip(*rows))
+
     plt.show()
 
 if __name__ == "__main__":
--- sqlite_utils.py
+++ sqlite_utils.py
@@ -9,7 +9,7 @@ class SQLiteDataAdapterConnection():
         self._adapter = adapter
         self._connection = lite.connect(self._adapter.database)
 
-    def execute_query(self, query):
+    def execute_select_query(self, query):
         with closing(self._connection.cursor()) as cursor:
             cursor.execute(query)
             rows = cursor.fetchall()
@@ -24,6 +24,7 @@ class SQLiteDataAdapterConnection():
 
         # Create bindings for column names
         bindings = ", ".join([':' + name for name in column_names])
+        print("Bindings: " + bindings)
 
         # Insert row data
         query = "INSERT INTO {0} VALUES({1})".format(table_name, bindings)
@@ -35,6 +36,7 @@ class SQLiteDataAdapterConnection():
         return self
 
     def __exit__(self, *args):
+        print("Closing connection")
         self._connection.commit()
         self._connection.close()
 
@@ -54,37 +56,25 @@ class SQLiteDataAdapter():
     def create_table(self, table_name):
         print("Creating table: {0}".format(table_name))
         with self._get_connection() as conn:
-            conn.execute_query("CREATE TABLE {0} (id INTEGER PRIMARY KEY, x REAL, y REAL)".format(table_name))
+            conn.execute_select_query("CREATE TABLE {0} (id INTEGER PRIMARY KEY, x REAL, y REAL)".format(table_name))
 
-    def create_test_table(self, table_name, number_of_rows):
-        self.create_table(table_name)
-        self.insert(table_name, self._get_rows(number_of_rows))
-
-    def _get_rows(self, num_rows):
-        i = 0
-        while i < num_rows:
-            x = i
-            y = math.sin(x + random.random()/10)
-            yield ({"id": None, "x": x, "y": y})
-            i += 1
-
-    def insert(self, table_name, column_dict):
+    def insert(self, table_name, rows_dict):
         with self._get_connection() as conn:
-            conn.execute_insert_query(table_name, column_dict)
+            conn.execute_insert_query(table_name, rows_dict)
 
     def get_tables(self):
         with self._get_connection() as conn:
-            result = conn.execute_query("SELECT * FROM sqlite_master WHERE type='table' LIMIT 1;")
+            result = conn.execute_select_query("SELECT * FROM sqlite_master WHERE type='table'")
         tables = [table[1] for table in result]
         return tables
 
     def get_table_schema(self, table_name):
         with self._get_connection() as conn:
             query = "PRAGMA table_info({0})".format(table_name)
-            result = conn.execute_query(query)
+            result = conn.execute_select_query(query)
         return result[0]
 
     def execute_query(self, query):
         with self._get_connection() as conn:
-            rows = conn.execute_query(query)
+            rows = conn.execute_select_query(query)
         return rows
